aa215e8fdca6418f739e19abf04f30ec842f7254
Kim Do Ky <makingfunk0@gmail.com>
2017-08-05 17:46:19 +0900

    setting url

--- kakaobot/broadcastBot/views.py
+++ kakaobot/broadcastBot/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+
+def product(request):
+    return JsonResponse({
+        'type': 'buttons',
+        'butons' : ['사슴', '잉어', '해마']
+        })
--- kakaobot/kakaobot/urls.py
+++ kakaobot/kakaobot/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from broadcastBot.views import product as pro
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^product/', pro),
 ]
