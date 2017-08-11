a827697620f57b8a54ec720470356238e26c3de2
Kim Do Ky <makingfunk0@gmail.com>
2017-08-11 21:16:27 +0900

    modify test image

--- kakaobot/broadcastBot/views.py
+++ kakaobot/broadcastBot/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json, datetime
+from IPython.display import Image
+
 
 def product(request):
     return JsonResponse({
@@ -8,6 +10,9 @@ def product(request):
         'buttons' : ['사슴', '잉어', '해마']
         })
 
+def testImage():
+    url = "http://mieninc.godohosting.com/chun/peach/p4.jpg"
+    return Image(url)
 
 @csrf_exempt
 def answer(request):
@@ -17,10 +22,11 @@ def answer(request):
     day = datetime.date.today()
     delivery = day + datetime.timedelta(days=4)
     delivery_date = delivery.strftime("%m월 %d일")
+    img = testImage()
 
     return JsonResponse({
         'message' : {
-            'text' : '문의 주신 상품은 ' + product_name + '입니다. 지금 주문하시면 배송은' + delivery_date + ' 도착 예정입니다.'
+            'text' : '문의 주신 상품은 ' + product_name + '입니다. 지금 주문하시면 배송은' + delivery_date + ' 도착 예정입니다.' + img
         },
         'keyboard' : {
             'type' : 'buttons',
